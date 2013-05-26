1a31695d549df0d85be22f34696fd23674dfc7c9
austinrivas <austinrivas@gmail.com>
2013-05-26 00:57:35 -0700

    updated package.js

--- package.js
+++ package.js
@@ -4,5 +4,5 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use('jquery', 'client');
-    api.add_files(['jquery.flot.min.js','jquery.flot.pie.min.js','jquery.flot.resize.min.js'], 'client');
+    api.add_files(['/lib/jquery.flot.min.js','/lib/jquery.flot.pie.min.js','/lib/jquery.flot.resize.min.js'], 'client');
 });
